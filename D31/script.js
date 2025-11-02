93c550564832fec9114fd33c1a65769719c679b2
Komal Bhagat <komalbhagat930@gmail.com>
2025-11-02 21:33:32 +0530

    D31 Operators

---
+++ D31/script.js
@@ -0,0 +1,54 @@
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+
+let x = 5;
+x += 3;
+console.log(x);
+x -= 3;
+console.log(x);
+x *= 3;
+console.log(x);
+x /= 3;
+console.log(x);
+
+
+let count = 5;
+console.log(count);
+
+count++;
+console.log(count);
+
+count--;
+console.log(count);
+
+
+console.log(5 == '5');
+console.log(5 === '5');
+
+
+console.log(10 > 5 && 10 < 20 && 10 == 10);
+
+console.log(true && false);
+console.log(true || false);
+console.log(!(true));
+
+
+test();
+function test(){
+    console.log("Hello");
+}
+
+
+hello();
+var hello =  function()
+    {
+        console.log("Hii");
+    }
