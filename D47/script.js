2bb61f4f311949236ae2c8b6ebd6b3570c702de9
Komal Bhagat <komalbhagat930@gmail.com>
2025-12-12 23:26:46 +0530

    D47 Practice

---
+++ D47/script.js
@@ -0,0 +1,23 @@
+let arr = [
+      {
+        user : "Komal",
+        city : "Ngp",
+      },
+      {
+        user : "Ram",
+        city : "Bhopal",
+      },
+      {
+        user : "Om",
+        city : "Indore"
+      }
+];
+
+console.log(arr[2].city);
+
+let name = ['Komal','Om','Ram','Ajay','Moksh'];
+
+ num.forEach(function(val){
+    sum += val;
+ })
+ console.log(sum);
