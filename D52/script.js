31d9da82d0e89e8ef1d2e80998dc9912ad144d92
Komal Bhagat <komalbhagat930@gmail.com>
2025-12-22 15:16:09 +0530

    D52 Spotlight Effect Started

---
+++ D52/script.js
@@ -0,0 +1,6 @@
+document.body.addEventListener('mousemove',(elem)=>{
+    console.log(elem.clientX,elem.clientY);
+    document.body.style.setProperty("--x",elem.clientX + "px");
+    document.body.style.setProperty("--y",elem.clientY + "px");
+
+})
