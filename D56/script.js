f6678bc8fd38485750d62b15928ca8519609c7dd
Komal Bhagat <komalbhagat930@gmail.com>
2026-01-07 08:44:13 +0530

    D56 OOPS Basics

---
+++ D56/script.js
@@ -0,0 +1,14 @@
+class Kitaab{
+    constructor(name,price,author,color){
+        this.name = name;
+        this.price = price;
+        this.author = author;
+        this.color = color;
+    }
+
+}
+
+let k1 = new Kitaab("Moral",342,"Harsh","Black");
+
+let k2 = new Kitaab("Wings of Fire",456,"A.P.J Abdul Kalam","Grey");
+ 
