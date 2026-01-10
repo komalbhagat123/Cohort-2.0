cd16cc5a05a4d6c7f93c088c92c62b6999fe8534
Komal Bhagat <komalbhagat930@gmail.com>
2026-01-10 20:04:57 +0530

    Added More Practice

--- D58/script2.js
+++ D58/script2.js
@@ -1,9 +1,73 @@
-function abcd() {
-    console.log(this.name);
+// function abcd() {
+//     console.log(this.name);
+// }
+
+// let obj = {
+//     name : "Harsh"
+// }
+
+// abcd.call(obj);
+
+const laptop = {
+    brand: "LENEVO",
+    price: 54000,
+    start: function () {
+        console.log("Laptop Started");
+    },
+
+    IncreasedPrice : function(){
+        newPrice = this.price + (this.price/10);
+        console.log(newPrice);
+    }
 }
 
-let obj = {
-    name : "Harsh"
+const laptop1 = {
+    brand: "HP",
+    price: 50000,
+    start: function () {
+        console.log("Laptop Started");
+    },
+
+    IncreasedPrice : function(){
+        newPrice = this.price + (this.price/10);
+        console.log(newPrice);
+    }
 }
 
-abcd.call(obj);
+///
+
+class Employee{
+    constructor(name,salary){
+        this.name = name,
+        this.salary = salary
+    }
+    showDetails(){
+        console.log(this.name + " "+this.salary);
+    }
+}
+
+let e1 = new Employee('Komal',50000);
+
+
+class BankAccount{
+    constructor(accountHolder,balance){
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    deposite(amount){
+        this.balance += amount;
+        console.log("Balance Added");
+    }
+}
+
+let ac1 = new BankAccount("Komal Bhagat",100);
+let ac2 = new BankAccount("Reshu Sharma",100);
+
+
+let profile = {
+    username : "Komal",
+    printName : function(){
+        console.log(this.username);
+    },
+}
